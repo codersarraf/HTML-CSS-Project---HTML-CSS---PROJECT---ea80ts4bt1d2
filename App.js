62fa10770407c2d65e24b885aa0fb5fd4d809ee3
amitkr <amitsarraf6@gmail.com>
2022-11-20 22:26:19 +0530

    Changed

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
+const otp = document.querySelectorAll(".otp");
 
+otp[0].focus();
+
+otp.forEach((inputs, index) => {
+  inputs.addEventListener("keydown", (e) => {
+    if (e.key >= 0 && e.key < 9) {
+      setTimeout(() => {
+        otp[index + 1].focus();
+      }, 10);
+    } else if (e.key === "Backspace") {
+      setTimeout(() => {
+        otp[index - 1].focus();
+      }, 10);
+    }
+  });
+});
+
+
+  
