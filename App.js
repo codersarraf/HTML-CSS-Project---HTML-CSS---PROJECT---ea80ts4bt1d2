e7918dfecc6c292201564f38bfd502103ef1d101
amitkr <amitsarraf6@gmail.com>
2022-12-14 17:00:21 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-const otp = document.querySelectorAll(".otp");
-
-otp[0].focus();
-
-otp.forEach((inputs, index) => {
-  inputs.addEventListener("keydown", (e) => {
-    if (e.key >= 0 && e.key < 9) {
-      setTimeout(() => {
-        otp[index + 1].focus();
-      }, 10);
-    } else if (e.key === "Backspace") {
-      setTimeout(() => {
-        otp[index - 1].focus();
-      }, 10);
-    }
-  });
-});
-
 
   
+const input0 = document.querySelector("#zero");
+const input1 = document.querySelector("#one");
+const input2 = document.querySelector("#two");
+const input3 = document.querySelector("#three");
+const input4 = document.querySelector("#four");
+const input5 = document.querySelector("#five");
+
+const inputs = [input0, input1, input2, input3, input4, input5];
+
+const moveFocusToNextInput = (eventOriginationInputNumber) => {
+  if (eventOriginationInputNumber === 5) {
+    return;
+  }
+
+  inputs[eventOriginationInputNumber + 1].focus();
+};
+
+const moveFocusToPreviousInput = (eventOriginationInputNumber) => {
+  if (eventOriginationInputNumber === 0) {
+    return;
+  }
+
+  inputs[eventOriginationInputNumber - 1].focus();
+};
+
+const onInputChange = (event) => {
+  const inputNumber = parseInt(event.target.getAttribute("data-number"));
+
+  if (event.key === "Backspace") {
+    moveFocusToPreviousInput(inputNumber);
+    return;
+  }
+
+  moveFocusToNextInput(inputNumber);
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", onInputChange);
+});
